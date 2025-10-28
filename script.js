5fd89b11f680087cb59c5f8cb0ab2891e4fa5693
christopher-besch <mail@chris-besch.com>
2025-10-28 16:56:08 +0100

    feat: html viewer different data

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
-function create_graph(data) {
-    // svg setup
-    const svg = d3.select("svg");
-    const width = Number(svg.attr("width"));
-    const height = Number(svg.attr("height"));
+// slightly transparent
+const hideLineColor = "#99999922";
+const defaultLineColor = "#999999ff";
+const highlightLineColor = "#999999ff";
 
-    // load data
-    const packages = data.packages.map((val) => ({ "id": val.replace("forgejo.org/", "") }));
-    const channels = data.channels.map((val) => ({ "id": val }));
-    const links = data.notifies.map((val) => ({ "source": val[0].replace("forgejo.org/", ""), "target": val[1].replace("forgejo.org/", "") }));
+// fully transparent
+const hideArrowColor = "#ffffff00";
+const defaultArrowColor = "#999999ff";
+const highlightArrowColor = "#999999ff";
 
-    // simulation setup
-    const simulation = d3.forceSimulation(packages.concat(channels))
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody().strength(-500))
-        .force("center", d3.forceCenter(width / 2, height / 2));
+const hidePackageColor = "#ffebddff";
+const defaultPackageColor = "#ff6600ff";
+const highlightPackageColor = "#ff6600ff";
 
-    // Do this after creating the simulation.
-    // Otherwise the id wouldn't have been populated yet.
-    let linkedLookup = {};
-    links.forEach(link => {
-        linkedLookup[`${link.source.id},${link.target.id}`] = true;
-        linkedLookup[`${link.target.id},${link.source.id}`] = true;
-    });
-    function isLinked(nodeA, nodeB) {
-        return nodeA.id === nodeB.id || linkedLookup[`${nodeA.id},${nodeB.id}`];
-    }
+const hideChannelColor = "#e1eff9ff";
+const defaultChannelColor = "#2185d0ff";
+const highlightChannelColor = "#2185d0ff";
 
-    // slightly transparent
-    const hideLineColor = "#99999922";
-    const defaultLineColor = "#999999ff";
-    const highlightLineColor = "#999999ff";
+const hideTextColor = "#00000022";
+const defaultTextColor = "#000000ff";
+const highlightTextColor = "#000000ff";
 
-    // fully transparent
-    const hideArrowColor = "#ffffff00";
-    const defaultArrowColor = "#999999ff";
-    const highlightArrowColor = "#999999ff";
+var simulation
+var timePoints
 
-    const hidePackageColor = "#ffebddff";
-    const defaultPackageColor = "#ff6600ff";
-    const highlightPackageColor = "#ff6600ff";
+var arrow
+var link
+var package
+var channel
+var label
 
-    const hideChannelColor = "#e1eff9ff";
-    const defaultChannelColor = "#2185d0ff";
-    const highlightChannelColor = "#2185d0ff";
+// svg setup
+const svg = d3.select("svg");
+const width = Number(svg.attr("width"));
+const height = Number(svg.attr("height"));
+const defs = svg.append("defs");
 
-    const hideTextColor = "#00000022";
-    const defaultTextColor = "#000000ff";
-    const highlightTextColor = "#000000ff";
-
+function populateSvg(data) {
     // arrowhead marker for lines
     // Create one for each link there is.
     // This allows us to set the color individually.
-    const defs = svg.append("defs");
-    const arrow = defs.selectAll("marker")
-        .data(links)
+    arrow = defs.selectAll("marker")
+        .data(data.links)
         .join("marker")
         .attr("id", (_d, i) => `arrowhead-${i}`)
         .attr("viewBox", "0 -5 10 10")
@@ -66,42 +53,53 @@ function create_graph(data) {
         .attr("d", "M0,-5L10,0L0,5")
         .attr("fill", defaultArrowColor);
     // edges
-    const link = svg.append("g")
+    link = svg.append("g")
         .attr("stroke", defaultLineColor)
         .style("color", defaultArrowColor)
         .attr("stroke-opacity", 0.6)
         .selectAll("line")
-        .data(links)
+        .data(data.links)
         .join("line")
         .attr("stroke-width", 1.5)
         .attr("marker-end", (_d, i) => `url(#arrowhead-${i})`);
     // nodes
-    const package = svg.append("g")
+    package = svg.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
         .selectAll("circle")
-        .data(packages)
+        .data(data.packages)
         .join("circle")
         .attr("r", 10)
         .attr("fill", defaultPackageColor);
-    const channel = svg.append("g")
+    channel = svg.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
         .selectAll("circle")
-        .data(channels)
+        .data(data.channels)
         .join("circle")
         .attr("r", 10)
         .attr("fill", defaultChannelColor);
     // node labels
-    const labels = svg.append("g")
+    label = svg.append("g")
         .selectAll("text")
-        .data(packages.concat(channels))
+        .data(data.packages.concat(data.channels))
         .join("text")
         .text(d => d.id)
         .attr("font-size", 10)
         .attr("fill", defaultTextColor)
         .attr("text-anchor", "middle")
         .attr("dy", -10);
+
+    // Do this after creating the simulation.
+    // Otherwise the id wouldn't have been populated yet.
+    let linkedLookup = {};
+    data.links.forEach(link => {
+        linkedLookup[`${link.source.id},${link.target.id}`] = true;
+        linkedLookup[`${link.target.id},${link.source.id}`] = true;
+    });
+    function isLinked(nodeA, nodeB) {
+        return nodeA.id === nodeB.id || linkedLookup[`${nodeA.id},${nodeB.id}`];
+    }
 
     function nodeMouseOver(_event, node) {
         package.attr("fill", other => (isLinked(node, other) ? highlightPackageColor : hidePackageColor));
@@ -117,7 +115,7 @@ function create_graph(data) {
             d3.select(`#arrowhead-${i} path`).attr("fill", arrowColor);
         });
 
-        labels.attr("fill", other => (isLinked(node, other) ? highlightTextColor : hideTextColor));
+        label.attr("fill", other => (isLinked(node, other) ? highlightTextColor : hideTextColor));
     }
     function nodeMouseOut(_event, _node) {
         // reset
@@ -125,7 +123,7 @@ function create_graph(data) {
         channel.attr("fill", defaultChannelColor);
         link.attr("stroke", defaultLineColor);
         arrow.attr("fill", defaultArrowColor);
-        labels.attr("fill", defaultTextColor);
+        label.attr("fill", defaultTextColor);
     }
     package.on("mouseover", nodeMouseOver)
         .on("mouseout", nodeMouseOut);
@@ -145,13 +143,54 @@ function create_graph(data) {
         channel
             .attr("cx", d => d.x)
             .attr("cy", d => d.y);
-        labels
+        label
             .attr("x", d => d.x)
             .attr("y", d => d.y - 10);
     });
 }
 
-fetch("1750504261_b2c4fc9f94e3ba2a5b5259dca4cf38b04701e574.json").
+function clearSvg() {
+    arrow.remove()
+    link.remove()
+    package.remove()
+    channel.remove()
+    label.remove()
+}
+
+function initSimulation(data) {
+    timePoints = data
+    // simulation setup
+    simulation = d3.forceSimulation(timePoints[0].packages.concat(timePoints[0].channels))
+        .force("link", d3.forceLink(timePoints[0].links).id(d => d.id))
+        .force("charge", d3.forceManyBody().strength(-500))
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    populateSvg(timePoints[0])
+}
+
+function updateSimulation(idx) {
+    simulation.nodes(timePoints[idx].packages.concat(timePoints[idx].channels))
+    simulation.force("link", d3.forceLink(timePoints[idx].links).id(d => d.id))
+    clearSvg()
+    populateSvg(timePoints[idx])
+
+    if (simulation.alpha() < 0.05) {
+        simulation.alphaTarget(0.2).restart();
+    }
+}
+
+fetch("forgejo_data.json").
     then(response => response.json()).
-    then(data => create_graph(data)).
+    then(data => {
+        console.log(data);
+        return data.map((dataPoint) => ({
+            "commit": dataPoint.commit,
+            "timestamp": dataPoint.timestamp,
+            "packages": dataPoint.packages.map((val) => ({ "id": val.replace("forgejo.org/", "").replace("code.gitea.io/gitea", "") })),
+            "channels": dataPoint.channels.map((val) => ({ "id": val })),
+            "links": dataPoint.notifies.map((val) => ({ "source": val[0].replace("forgejo.org/", "").replace("code.gitea.io/gitea", ""), "target": val[1].replace("forgejo.org/", "").replace("code.gitea.io/gitea", "") })),
+        })
+        );
+    }).
+    then(data => initSimulation(data)).
     catch(error => console.error(error))
