d735d3223ae0e383a8e311dcc270c6125ec042f5
christopher-besch <mail@chris-besch.com>
2025-10-29 10:09:16 +0100

    fix: xorm version weirdness

--- auto_parser.sh
+++ auto_parser.sh
@@ -13,10 +13,15 @@ while true; do
         echo "skipping as this commit doesn't change any .go files"
     fi
 
-    # We need to download the old tools.
+    # We need to download the old tools because at some point the new tools fail to work with the old repo.
     go install "golang.org/dl/go$(cat go.mod | grep -P '^go ' | cut -d ' ' -f2)@latest"
     "go$(cat go.mod | grep -P '^go ' | cut -d ' ' -f2)" download
-    # ed1d95c55dfa91d1c9a486bfb8e00375d4038e29 repairs something that makes loading fail otherwise, solution:
+
+    # 5efd3630bc21d4b0ba6ff492d16d4c7e2814dd1f updates to xorm v0.7.4
+    # Before `go mod tidy` didn't work.
+    sed -i 's#github.com/go-xorm/xorm v0.7.3-0.20190620151208-f1b4f8368459#github.com/go-xorm/xorm v0.7.3#' go.mod
+
+    # ed1d95c55dfa91d1c9a486bfb8e00375d4038e29 repairs something that makes loading fail otherwise, solution: go mod tidy
     "go$(cat go.mod | grep -P '^go ' | cut -d ' ' -f2)" mod tidy
 
     # Apparently the go dir can grow to absurd sizes; prevent that.
