1bc3b332f3d71f04467b2d66b6df48ae9d9f1fe5
christopher-besch <mail@chris-besch.com>
2025-10-29 13:03:27 +0100

    feat: output one empty file in the end

--- parser/main.go
+++ parser/main.go
@@ -188,13 +188,15 @@ func main() {
 	for channel := range out_channels {
 		graph_out.Channels = append(graph_out.Channels, channel)
 	}
-	if len(graph_out.Channels) == 0 || len(graph_out.Packages) == 0 {
-		log.Fatal("failed to find any channels or packages")
-	}
 
 	json_data, err := json.Marshal(graph_out)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(json_data))
+
+	// Fail after creating the output file.
+	if len(graph_out.Channels) == 0 || len(graph_out.Packages) == 0 {
+		log.Fatal("failed to find any channels or packages")
+	}
 }
